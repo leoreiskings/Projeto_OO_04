5bf7046845a2ee49f549c7f30f213e9c2491400f
leoreiskings <leo19reis@gmail.com>
2023-08-28 10:44:45 -0300

    commitando o arquivo ConnectionFactory.java

--- src/factories/ConnectionFactory.java
+++ src/factories/ConnectionFactory.java
@@ -8,10 +8,11 @@ public class ConnectionFactory {
 	// método para abrir e retornar uma conexão com o MySQL
 	public static Connection getConnection() throws Exception {
 		
+
 	// parametros para conexão com o MYSQL
-	String host = "jdbc:mysql://localhost:3306/bd_projetoaula04?useTimezone=true&serverTimezone=UTC&useSSL=false";
-	String user = "root";
-	String pass = "reis";
+	String host = "xxxxxx";
+	String user = "xxxxxx";
+	String pass = "xxxxxx";
 	
 	//abrindo e retornando a conexão com o banco de dados
 	return DriverManager.getConnection(host, user, pass);
